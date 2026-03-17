3a6e39bfecbe36b0abfb3c735311ac05db7c881f
Deep Ghosh <deepghosh_tyco@gmail.com>
2026-03-17 15:19:55 +0530

    Clean history - Complete setup and prepare base model

---
+++ src/cnnClassifier/constants/__init__.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+# Tumhare PC ka exact absolute path
+PROJECT_ROOT = Path(r"C:\Users\Admin\Downloads\kidney-diseases-classification")
+
+CONFIG_FILE_PATH = PROJECT_ROOT / "config" / "config.yaml"
+PARAMS_FILE_PATH = PROJECT_ROOT / "params.yaml"
